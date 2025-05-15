3bd993e793d3992b9fd00d778b7c66643191ea54
CircuitSacul <me@circuitsacul.dev>
2025-05-15 10:12:02 -0400

    only create other windows when needed

--- yazi/yazihx.sh
+++ yazi/yazihx.sh
@@ -12,11 +12,4 @@ else
   # start on the Yazi window
   tmux select-window -t "$sess:tree"
   tmux attach -t "$sess"
-
-  # second window runs Helix in project root
-  tmux new-window -t "$sess" -n edit "cd \"$proj\"; hx ."
-
-  # lazygit
-  tmux new-window -t "$sess" -n lazygit "cd \"$proj\"; lazygit"
-
 fi
