8b5b1057827a4ed65195ebddf4b2cb2f7ae46f83
CircuitSacul <me@circuitsacul.dev>
2025-07-15 09:57:11 -0400

    colemak-dh

--- helix/runtime/queries/rust/injections.scm
+++ helix/runtime/queries/rust/injections.scm
@@ -25,3 +25,29 @@
  (#eq? @_macro_name "rsx")
  (#set! injection.language "rust-format-args")
  (#set! injection.include-children))
+
+; lua
+((macro_invocation
+   macro:
+     [
+       (scoped_identifier
+         name: (_) @_macro_name)
+       (identifier) @_macro_name
+     ]
+   (token_tree) @injection.content)
+ (#eq? @_macro_name "chunk")
+ (#set! injection.language "luau")
+ (#set! injection.include-children))
+
+; tokio select!
+((macro_invocation
+   macro:
+     [
+       (scoped_identifier
+         name: (_) @_macro_name)
+       (identifier) @_macro_name
+     ]
+   (token_tree) @injection.content)
+ (#eq? @_macro_name "select")
+ (#set! injection.language "rust")
+ (#set! injection.include-children))
